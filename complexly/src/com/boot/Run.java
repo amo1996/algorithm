b7dd37bdcf7654a0670fdfc1144212e5531bdd41
lzlz <lzlz1996@126.com>
2022-03-23 22:12:01 +0800

    加解释

---
+++ complexly/src/com/boot/Run.java
@@ -0,0 +1,18 @@
+package com.boot;
+
+/**
+ * @Classname Run
+ * @Description
+ * @Date 2022/3/23 22:11
+ * @Created by liZhi
+ */
+public class Run {
+
+    /**
+     * @Description
+     * @author liZhi
+     * 1. 从spring.factories配置文件中加载EventPublishingRunListener对象，
+     * 该对象拥有SimpleApplicationEventMulticaster属性，
+     * 即在SpringBoot启动过程的不同阶段用来发射内置的生命周期事件;
+    */
+}
