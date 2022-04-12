2c9ebc2b1e0c8b88157a644a9842a7c10dfef870
lzlz <lzlz1996@126.com>
2022-04-12 14:17:44 +0800

    大文件上传

--- complexly/src/com/boot/Run.java
+++ complexly/src/com/boot/Run.java
@@ -14,6 +14,6 @@ public class Run {
      * 1. 从spring.factories配置文件中加载EventPublishingRunListener对象，
      * 该对象拥有SimpleApplicationEventMulticaster属性，
      * 即在SpringBoot启动过程的不同阶段用来发射内置的生命周期事件;
-     * 2. 大文件分片上传  断点重续   md5算法  node-js
+     * 2. 大文件.分片上传  断点重续   md5算法  node-js   guava
     */
 }
