26fd88a320b2771be4d2c8f9b95873bcd3645b13
lzlz <lzlz1996@126.com>
2022-06-27 19:59:54 +0800

    测试jdk版本号

--- complexly/src/com/boot/Run.java
+++ complexly/src/com/boot/Run.java
@@ -16,4 +16,10 @@ public class Run {
      * 即在SpringBoot启动过程的不同阶段用来发射内置的生命周期事件;
      * 2. 大文件.分片上传  断点重续   md5算法  node-js   guava  hdfs
     */
+
+    public static void main(String[] args) {
+
+        System.out.println("测试jdk1.8");
+
+    }
 }
